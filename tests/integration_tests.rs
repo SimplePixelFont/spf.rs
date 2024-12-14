5de950131b6e85d7204990061a09a33c8a415df9
The-Nice-One <nicedude618@gmail.com>
2024-12-14 14:07:03 -0500

    Modularizing Tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,8 +1,9 @@
+mod common;
+
 use spf::core::*;
-use std::io::{Read, Write};
 
 #[test]
-fn create_font_file() -> Result<(), String> {
+fn write_font_file() -> Result<(), String> {
     let mut font = SimplePixelFont::new(
         ConfigurationFlags {
             0: ALIGNMENT_HEIGHT,
@@ -35,24 +36,14 @@ fn create_font_file() -> Result<(), String> {
         ]),
     ))?;
 
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open("./res/sampleToyFont.spf")
-        .unwrap();
-    file.write_all(&font.to_vec_u8()).unwrap();
-
+    common::write_to_file("./res/sampleToyFont.spf", &font.to_vec_u8()).unwrap();
     Ok(())
 }
 
 #[test]
 fn read_font_file() -> Result<(), String> {
     let mut buffer: Vec<u8> = vec![];
-    let mut file = std::fs::OpenOptions::new()
-        .read(true)
-        .open("./res/sampleToyFont.spf")
-        .unwrap();
-    file.read_to_end(&mut buffer).unwrap();
+    common::read_from_file("./res/sampleToyFont.spf", &mut buffer).unwrap();
 
     let _font = SimplePixelFont::from_vec_u8(buffer);
 
