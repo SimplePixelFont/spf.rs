83ccda19a7ffb5ff3acfe7667f024bfec33acf09
The-Nice-One <>
2024-12-28 14:40:18 -0500

    Last Sync: 2024-12-28 19:40:18 (Mobile) - Documentation Rework

--- src/articles.rs
+++ src/articles.rs
@@ -2,5 +2,7 @@
 //! These modules contain helpful recipes to integrate spf.rs with your rendering engine.
 //! They also contain some learning resources, and guides to help you get started.
 
-/// Guide for integrating spf.rs with miniquad/macroquad.
-pub mod miniquad_integration;
+/// Guide for integrating spf.rs with Miniquad/Macroquad.
+pub mod miniquad_integration {
+    #![doc = include_str!("../res/articles/MiniquadIntegration.md")]
+};
--- src/articles/miniquad_integration.rs
+++
@@ -1,8 +0,0 @@
-//! # Miniquad/Macroquad Integration
-//! We will first start with miniquad integration, for macroquad integration skip
-//! to the next section.
-//! To begin with install spf, miniquad, and macroquad to your project via:
-//! ```sh
-//! cargo add spf
-//! cargo add miniquad
-//! ```
