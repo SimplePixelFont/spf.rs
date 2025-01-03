211072f6ac7b97a63a779f84e690d72590c65af1
The-Nice-One <>
2025-01-02 20:16:39 -0500

    Last Sync: 2025-01-03 01:16:39 (Mobile) - Push articles (unfinished)

--- src/articles.rs
+++ src/articles.rs
@@ -2,7 +2,12 @@
 //! These modules contain helpful recipes to integrate spf.rs with your rendering engine.
 //! They also contain some learning resources, and guides to help you get started.
 
-/// Guide for integrating spf.rs with Miniquad/Macroquad.
+/// Specific Guide for integrating spf.rs with Miniquad/Macroquad projects.
 pub mod miniquad_integration {
     #![doc = include_str!("../res/articles/MiniquadIntegration.md")]
+}
+
+/// Guide for spf.rs basics and understanding the file format via Rust.
+pub mod getting_started {
+    #![doc = include_str!("../res/articles/GettingStarted.md")]
 }
