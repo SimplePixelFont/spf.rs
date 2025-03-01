9274ad68bd5006581cb34237ad1838c3cccd49a0
The-Nice-One <nicedude618@gmail.com>
2025-03-01 12:02:05 -0500

    added articles to articles.md + changed to rlib

--- src/articles.rs
+++ src/articles.rs
@@ -1,12 +1,22 @@
 //! Articles
 //! These modules contain helpful guides and learning resources to integrate spf.rs in your next project.
 
-/// Specific Guide for integrating spf.rs with Miniquad/Macroquad projects.
-pub mod miniquad_integration {
-    #![doc = include_str!("../res/articles/MiniquadIntegration.md")]
+/// Guide for `spf.rs` printer module and creating text renderings.
+pub mod printer_guide {
+    #![doc = include_str!("../res/articles/PrinterModuleAndTextRenderings.md")]
 }
 
 /// Guide for spf.rs basics and understanding the file format via Rust.
 pub mod getting_started {
     #![doc = include_str!("../res/articles/GettingStartedInRust.md")]
+}
+
+/// Guide for installing `spf.rs` crate and dynamic FFI library.
+pub mod installing {
+    #![doc = include_str!("../res/articles/InstallingSPFRS.md")]
+}
+
+/// Guide for using `spf.rs` as an FFI library in C.
+pub mod c_usage {
+    #![doc = include_str!("../res/articles/UsingSPFRSWithFFI.md")]
 }
