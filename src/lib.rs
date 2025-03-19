b0afa29cdfef24d960ca4e1ea766be561811cce2
The-Nice-One <>
2025-03-18 22:32:39 -0400

    Last Sync: 2025-03-19 02:32:39 (Mobile) - test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 #![doc(
     html_logo_url = "https://github.com/SimplePixelFont/spf.rs/blob/main/res/spf.rs.png?raw=true"
 )]
-
+/// test
 pub mod core;
 
 #[cfg_attr(docsrs, doc(cfg(feature = "log")))]
