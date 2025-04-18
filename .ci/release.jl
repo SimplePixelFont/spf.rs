82fa8fab00c96c6f37454c32d9864585b2426ac6
The-Nice-One <nicedude618@gmail.com>
2025-04-17 20:51:53 -0400

    debug (7)

--- .ci/release.jl
+++ .ci/release.jl
@@ -5,10 +5,11 @@ println(readdir())
 println(Base.julia_cmd())
 println("\n\n")
 # Build for MacOS targets
-cmd = `BINARYBUILDER_AUTOMATIC_APPLE=true $(Base.julia_cmd()) -- build_tarballs.jl "x86_64-apple-darwin", "aarch64-apple-darwin"`
+julia = "/home/runner/.julia/juliaup/julia-1.7.0+0.x64.linux.gnu/bin/julia"
+cmd = `BINARYBUILDER_AUTOMATIC_APPLE=true $julia -- build_tarballs.jl "x86_64-apple-darwin", "aarch64-apple-darwin"`
 
 println("Running command: $cmd")
 run(cmd)
 
 # Build for Linux targets
-run(`$(Base.julia_cmd()) -- build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
+run(`$julia -- build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
