9d6970c6446dd7056fd609f2b6aedf790311e022
The-Nice-One <nicedude618@gmail.com>
2025-04-17 21:21:11 -0400

    debug (12)

--- .ci/release.jl
+++ .ci/release.jl
@@ -1,12 +1,11 @@
-cd(".ci")
 # Build for MacOS targets
-run(`sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- build_tarballs.jl x86_64-apple-darwin, aarch64-apple-darwin"`)
+run(`sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./.ci/build_tarballs.jl \"x86_64-apple-darwin\", \"aarch64-apple-darwin\""`)
 
 # Build for Linux targets
-run(`sh -c "julia -- build_tarballs.jl i686-linux-gnu, x86_64-linux-gnu, aarch64-linux-gnu, armv6l-linux-gnueabihf, armv7l-linux-gnueabihf, powerpc64le-linux-gnu, i686-linux-musl, x86_64-linux-musl, aarch64-linux-musl, armv6l-linux-musleabihf, armv7l-linux-musleabihf, x86_64-unknown-freebsd"`)
+run(`sh -c "julia -- ./.ci/build_tarballs.jl i686-linux-gnu, x86_64-linux-gnu, aarch64-linux-gnu, armv6l-linux-gnueabihf, armv7l-linux-gnueabihf, powerpc64le-linux-gnu, i686-linux-musl, x86_64-linux-musl, aarch64-linux-musl, armv6l-linux-musleabihf, armv7l-linux-musleabihf, x86_64-unknown-freebsd"`)
 
 # Build for windows-gnu target
-run(`sh -c "julia -- build_tarballs.jl x86_64-w64-mingw32"`)
+run(`sh -c "julia -- ./.ci/build_tarballs.jl x86_64-w64-mingw32"`)
 
 # Build for windows-msvc target
 run(`sh -c "cargo install --locked cargo-xwin"`)
