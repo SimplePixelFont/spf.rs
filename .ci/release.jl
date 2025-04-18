aeadb106ba1cfe71458ee7b22e322283cddd3c40
The-Nice-One <nicedude618@gmail.com>
2025-04-17 20:48:19 -0400

    debug (5)

--- .ci/release.jl
+++ .ci/release.jl
@@ -3,8 +3,12 @@ println(readdir())
 cd(".ci")
 println(readdir())
 println(Base.julia_cmd())
+println(\n\n)
 # Build for MacOS targets
-run(`BINARYBUILDER_AUTOMATIC_APPLE=true $(Base.julia_cmd()) -- build_tarballs.jl "x86_64-apple-darwin", "aarch64-apple-darwin"`)
+cmd = `BINARYBUILDER_AUTOMATIC_APPLE=true $(Base.julia_cmd()) -- build_tarballs.jl "x86_64-apple-darwin", "aarch64-apple-darwin"`
+
+println("Running command: $cmd")
+run(cmd)
 
 # Build for Linux targets
 run(`$(Base.julia_cmd()) -- build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
