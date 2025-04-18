2e927cd753735cf27b12b6921940e46d5d6bbe4b
The-Nice-One <nicedude618@gmail.com>
2025-04-18 07:47:04 -0400

    testing

--- .ci/release.jl
+++ .ci/release.jl
@@ -2,14 +2,34 @@
 # Build for MacOS targets
 run(`sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./.ci/build_tarballs.jl 'x86_64-apple-darwin','aarch64-apple-darwin'"`)
 
+jl_platforms = [
+    "'i686-linux-gnu'"
+    "'x86_64-linux-gnu'"
+    "'aarch64-linux-gnu'"
+    "'armv6l-linux-gnueabihf'"
+    "'armv7l-linux-gnueabihf'"
+    "'powerpc64le-linux-gnu'"
+    "'i686-linux-musl'"
+    "'x86_64-linux-musl'"
+    "'aarch64-linux-musl'"
+    "'armv6l-linux-musleabihf'"
+    "'armv7l-linux-musleabihf'"
+    "'x86_64-unknown-freebsd'"
+]
+
+for platform in jl_platforms
+    run(`sh -c "julia -- ./.ci/build_tarballs.jl $platform"`)
+end
+
+
 # Build for Linux GNU targets
-run(`sh -c "julia -- ./.ci/build_tarballs.jl 'i686-linux-gnu','x86_64-linux-gnu','aarch64-linux-gnu'"`)
+#run(`sh -c "julia -- ./.ci/build_tarballs.jl 'i686-linux-gnu','x86_64-linux-gnu','aarch64-linux-gnu'"`)
 
 # Build for Linux MUSL targets
-run(`sh -c "julia -- ./.ci/build_tarballs.jl 'i686-linux-musl','x86_64-linux-musl','aarch64-linux-musl'"`)
+#run(`sh -c "julia -- ./.ci/build_tarballs.jl 'i686-linux-musl','x86_64-linux-musl','aarch64-linux-musl'"`)
 
 # Build for extra Linux targets
-run(`sh -c "julia -- ./.ci/build_tarballs.jl 'armv6l-linux-gnueabihf','armv7l-linux-gnueabihf','armv6l-linux-musleabihf','armv7l-linux-musleabihf','x86_64-unknown-freebsd'"`)
+#run(`sh -c "julia -- ./.ci/build_tarballs.jl 'armv6l-linux-gnueabihf','armv7l-linux-gnueabihf','armv6l-linux-musleabihf','armv7l-linux-musleabihf','x86_64-unknown-freebsd'"`)
 
 # Build for Windows targets
-run(`sh -c "julia -- ./.ci/build_tarballs.jl 'x86_64-w64-mingw32'"`)
+#run(`sh -c "julia -- ./.ci/build_tarballs.jl 'x86_64-w64-mingw32'"`)
