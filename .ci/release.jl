b208172fabf376c4d3029cbe12f0b86466ae9bcf
The-Nice-One <nicedude618@gmail.com>
2025-04-17 21:12:23 -0400

    debug (9)

--- .ci/release.jl
+++ .ci/release.jl
@@ -1,15 +1,17 @@
-
-println(readdir())
-cd(".ci")
-println(readdir())
-println(Base.julia_cmd())
-println("\n\n")
 # Build for MacOS targets
-julia = "/home/runner/.julia/juliaup/julia-1.7.0+0.x64.linux.gnu/bin/julia"
-cmd = `sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- build_tarballs.jl x86_64-apple-darwin, aarch64-apple-darwin"`
-
-println("Running command: $cmd")
-run(cmd)
+run(`sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./.ci/build_tarballs.jl x86_64-apple-darwin, aarch64-apple-darwin"`)
 
 # Build for Linux targets
-run(`$julia -- build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
+run(`sh -c "julia -- ./.ci/build_tarballs.jl i686-linux-gnu, x86_64-linux-gnu, aarch64-linux-gnu, armv6l-linux-gnueabihf, armv7l-linux-gnueabihf, powerpc64le-linux-gnu, i686-linux-musl, x86_64-linux-musl, aarch64-linux-musl, armv6l-linux-musleabihf, armv7l-linux-musleabihf, x86_64-unknown-freebsd"`)
+
+# Build for windows-gnu target
+run(`sh -c "julia -- ./.ci/build_tarballs.jl x86_64-w64-mingw32"`)
+
+# Build for windows-msvc target
+run(`sh -c "cargo install --locked cargo-xwin"`)
+run(`sh -c "rustup target add x86_64-pc-windows-msvc"`)
+run(`sh -c "cargo xwin build --target x86_64-pc-windows-msvc --release"`)
+
+# Build for WASM target
+run(`sh -c "rustup target add wasm32-unknown-unknown"`)
+run(`sh -c "cargo build --target wasm32-unknown-unknown --release"`)
