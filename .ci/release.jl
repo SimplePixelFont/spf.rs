6ae289a9b6c558964ef32b49f0723aa6187d0ea8
The-Nice-One <nicedude618@gmail.com>
2025-04-17 22:08:07 -0400

    update build scripts again.

--- .ci/release.jl
+++ .ci/release.jl
@@ -1,10 +1,6 @@
 
 # Build for MacOS targets
-cmd = `sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./.ci/build_tarballs.jl 'x86_64-apple-darwin','aarch64-apple-darwin'"`
-
-println("Running command: $cmd")
-run(cmd)
 run(`sh -c "BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./.ci/build_tarballs.jl 'x86_64-apple-darwin','aarch64-apple-darwin'"`)
 
 # Build for Linux targets
-run(`$julia -- build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
+run(`sh -c "julia -- ./.ci/build_tarballs.jl 'i686-linux-gnu','x86_64-linux-gnu','aarch64-linux-gnu','armv6l-linux-gnueabihf','armv7l-linux-gnueabihf','powerpc64le-linux-gnu','i686-linux-musl','x86_64-linux-musl','aarch64-linux-musl','armv6l-linux-musleabihf','armv7l-linux-musleabihf','x86_64-unknown-freebsd'"`)
