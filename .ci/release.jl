e44e0e2a034d1aa1216739d42980ffb6617a8d84
The-Nice-One <nicedude618@gmail.com>
2025-04-17 20:03:36 -0400

    fixed paths?

--- .ci/release.jl
+++ .ci/release.jl
@@ -1,6 +1,6 @@
 
 # Build for MacOS targets
-run(`BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./build_tarballs.jl "x86_64-apple-darwin", "aarch64-apple-darwin"`)
+run(`BINARYBUILDER_AUTOMATIC_APPLE=true julia -- ./.ci/build_tarballs.jl "x86_64-apple-darwin", "aarch64-apple-darwin"`)
 
 # Build for Linux targets
-run(`julia -- ./build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
+run(`julia -- ./.ci/build_tarballs.jl "i686-linux-gnu", "x86_64-linux-gnu", "aarch64-linux-gnu", "armv6l-linux-gnueabihf", "armv7l-linux-gnueabihf", "powerpc64le-linux-gnu", "i686-linux-musl", "x86_64-linux-musl", "aarch64-linux-musl", "armv6l-linux-musleabihf", "armv7l-linux-musleabihf", "x86_64-unknown-freebsd"`)
