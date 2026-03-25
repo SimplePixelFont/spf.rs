f6236d4c6cd1258bc83b1833f0cc9f1815dae6ed
The-Nice-One <nicedude618@gmail.com>
2026-03-24 20:49:30 -0400

    add seperators to builder debug output

--- .ci/build_tarballs.jl
+++ .ci/build_tarballs.jl
@@ -25,9 +25,13 @@ cd $WORKSPACE/srcdir
 cd spf.rs
 mkdir target
 cargo rustc --release --no-default-features --features "ffi,std" --crate-type cdylib --crate-type staticlib -- -C target-feature=-crt-static
+echo "------"
 ls target
+echo "------"
 ls target/release
+echo "------"
 echo ${rust_target}
+echo "------"
 
 if [[ "${rust_target}" == "x86_64-pc-windows-gnu" ]]; then
     install -D -m 755 "target/${rust_target}/release/spf.${dlext}" "${libdir}/libspf.${dlext}"
